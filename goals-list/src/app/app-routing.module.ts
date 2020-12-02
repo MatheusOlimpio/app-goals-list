83f61feb9fe7a92357a1a1e1f1c973f4b5145874
Matheus Olimpio de Oliveira <mah.oliver_crz@hotmail.com>
2020-12-01 21:40:30 -0300

    Adicionando modulos e rotas dos componentes criados ao angular.

--- goals-list/src/app/app-routing.module.ts
+++ goals-list/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GoalAddComponent } from './goal-add/goal-add.component';
+import { GoalEditComponent } from './goal-edit/goal-edit.component';
+import { GoalListComponent } from './goal-list/goal-list.component';
+
+const routes: Routes = [
+  { path: 'goals/create', component: GoalAddComponent },
+  { path: 'goals/:id', component: GoalEditComponent },
+  { path: 'goals', component: GoalListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
